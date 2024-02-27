2351b07110a7a2eefee5aecc53e9f1801d9f9a85
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-02-27 16:18:07 -0300

    Aula sobre comentários

---
+++ comentario.js
@@ -0,0 +1,10 @@
+// Aqui eu tenho um comentário em linha
+
+/*
+Aqui eu tenho um comentario multi linha ou de bloco
+
+o codigo abaixo escreve alguma mensagem no devtools
+*/
+
+console.log('Bem vindos ao Starter')
+console.log('não iria ignorar')
