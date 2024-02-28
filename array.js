f6d991eac53fcd32f874ebf753ef7f478f7b8fb8
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-02-28 18:10:49 -0300

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+/*
+* Array(Vetores)
+    * Uma lista
+    * Agrupamento de dado
+    * Tipo de Dado estrutural
+    * ['César', 23]
+*/
+console.log([
+    'Leite',
+    'Ovos',
+    2,
+    3
+])
