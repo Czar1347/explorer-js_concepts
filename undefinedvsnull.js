2511b765512033fae06f16b9d0cd451ded8c3637
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-02-28 16:10:02 -0300

    undefined vs null

---
+++ undefinedvsnull.js
@@ -0,0 +1,10 @@
+/*
+    undefined
+        indefiniddo
+    null
+        nulo
+        objeto que não possui nada dentro
+        diferente de indefinido
+    null é algo que existe mas esta vazio, já undefined seria algo que não existe
+*/
+
