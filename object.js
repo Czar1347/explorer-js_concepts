a742510eee6af99c117131f2d1a8096d43842fb5
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-02-28 18:02:27 -0300

    object

---
+++ object.js
@@ -0,0 +1,17 @@
+/*
+* Object
+    *Objeto
+    *Propriedade / Atributos
+    *Funcionalidades / Métodos
+
+{ propriedade: "valor"}
+É um tipo de dados estrutural
+*/
+
+console.log({
+  name: "César",
+  idade: 23,
+  andar: function () {
+    console.log("andar");
+  },
+});
