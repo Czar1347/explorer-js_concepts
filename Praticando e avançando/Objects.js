cde972db160d82772bb9b71bd49c2b0e7e258e1d
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-03 20:49:50 -0300

    Objects

---
+++ Praticando e avançando/Objects.js
@@ -0,0 +1,11 @@
+// Object
+
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+// para invocar alguma propriedade dentro do objeto
+console.log(`${person.name} tem ${person.age} anos`)
