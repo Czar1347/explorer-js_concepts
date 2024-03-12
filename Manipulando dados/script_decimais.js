0dfe10a727185e854c978338b2b9560ba50f0653
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-12 05:29:34 -0300

    Maiúsculas e minúsculas

---
+++ Manipulando dados/script_decimais.js
@@ -0,0 +1,5 @@
+// Manipulando Strings e Números
+
+//Transformar um  npumero quebrado com 2 casas decimais e trocar ponto por vírgula
+let number = 123.234523
+console.log(number.toFixed(2).replace('.',"")) // toFixed é uma função atrelada ou método
