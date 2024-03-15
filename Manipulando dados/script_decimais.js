1cbfd945521c23feb3253e67d621dcc32a7c9576
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-15 12:45:10 -0300

    Encontrando palavras em frases

--- Manipulando dados/script_decimais.js
+++ Manipulando dados/script_decimais.js
@@ -2,4 +2,4 @@
 
 //Transformar um  npumero quebrado com 2 casas decimais e trocar ponto por vírgula
 let number = 123.234523
-console.log(number.toFixed(2).replace('.',"")) // toFixed é uma função atrelada ou método
+console.log(number.toFixed(2).replace('.',",")) // toFixed é uma função atrelada ou método
---
+++ Manipulando dados/script_words.js
@@ -0,0 +1,6 @@
+// Manipulando Strigs
+
+//Verificar se o texto contém a palavra Amor
+
+let phrase = "Eu quero viver o amor !"
+console.log(phrase.toLowerCase().includes("amor"))
