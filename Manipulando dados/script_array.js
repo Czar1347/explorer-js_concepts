7404132f61074ebdb623e7cec3568a29f4fce01f
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-15 12:51:42 -0300

    Criando array com construtor

---
+++ Manipulando dados/script_array.js
@@ -0,0 +1,9 @@
+// Manipulando Arrays
+
+
+// Criar Array com construtor
+
+// let myArray = ['a', 'b', 'c']
+// let myArray = new Array('a', 'b', 'c')
+let myArray = new Array(10)
+console.log(myArray)
