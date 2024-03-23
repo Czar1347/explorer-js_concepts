51d8a1eda6b7e2aa7deb26317ba1f02316ca10b1
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-23 19:54:14 -0300

    Operadores de aribuição

---
+++ Expressoes e Operadores/Operadores_de_atribuicao.js
@@ -0,0 +1,27 @@
+// Operadores de atribuição (Assignment)
+let x;
+
+//console.log(x)
+
+// assignment
+x = 1;
+
+//addition assignment
+// x = x + 2
+x += 2;
+console.log(x);
+
+//subtraction assignment
+//x = x - 1
+x -= 1
+
+//multipliocation assignment
+//x = x * 2
+x*=2
+
+// division assignment
+// x = x / 2
+
+//remainder, exponetiation
+// x %= 2
+// x **=2
