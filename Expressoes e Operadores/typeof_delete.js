f99cb013246d7db6f5e1031df9cfae7a310fadc6
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-24 10:18:50 -0300

    Typeof Delete

---
+++ Expressoes e Operadores/typeof_delete.js
@@ -0,0 +1,15 @@
+/*
+    Operadores unários
+    typeof
+    delete
+
+*/
+// console.log(typeof 'César')
+
+const person = {
+    name: 'César',
+    age: 25
+}
+delete person.age
+
+console.log(person)
