11a7c764f9cd871abb91e1be73376d89e7554d9b
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-26 19:32:00 -0300

    For of

---
+++ Estruturas de repetição/For_of.js
@@ -0,0 +1,12 @@
+// for...of
+
+let name = 'César'
+let names = ['João', 'Paulo', 'Pedro']
+
+// for(let char of name){
+//     console.log(char)
+// }
+
+for(let name of names){
+    console.log(name)
+}
